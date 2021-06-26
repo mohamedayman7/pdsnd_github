9c836a474c8dc05434fb82da73d157aa72ebfb7f
Mohamed-Ayman <m.ayman.19970@gmail.com>
2021-06-26 12:07:24 +0200

    Ayman add show more function to bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -198,6 +198,14 @@ def user_stats(df):
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-' * 40)
 
+def show_more(df):
+    view_data = input('\nWould you like to view 5 rows of individual trip data? Enter yes or no\n').lower()
+    start_loc = 0
+    while view_data == 'yes':
+        print(df.iloc[start_loc:start_loc+5])
+        start_loc += 5
+        view_data = input("Do you wish to continue? Enter yes or no:").lower()
+
 
 
 def main():
@@ -209,7 +217,7 @@ def main():
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df)
-        
+        show_more(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
